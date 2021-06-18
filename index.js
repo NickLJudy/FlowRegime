514b60e7b31ea3a23920b40306075e77683e1e3f
yangguang26 <yangguang26@maoyan.com>
2021-06-18 15:35:35 +0800

    init

---
+++ index.js
@@ -0,0 +1,78 @@
+import { createElement, createContext, useReducer, useContext,} from 'react';
+import { isType, variableRelation, compose, } from './util';
+
+let ctrlState;
+const SingleContextType = Symbol("A separate context type.");
+const repos = new Map([[SingleContextType, createContext()]]);
+const isMulti = str => String(str).indexOf('MULTI-') === 0;
+
+function StateWrapper({ children }) {
+  const [state, dispatch] = useReducer(reducer, { types: new Set([SingleContextType]) });
+
+  function reducer(state, action) {
+    let { type, value } = action;
+    let { types } = state;
+
+    types.add(type);
+
+    if (!value) {
+      repos.set(type, createContext());
+
+      return state;
+    }
+    const Relation = variableRelation(state[type], value);
+    let obj = {}
+
+    if (Relation === 'same') throw new Error('the state shouldn\'t appear in dispatch.');
+    if (Relation !== 'different') return state;
+
+    obj[type] = isType(state[type]) && isType(value) ? { ...state[type], ...value } : value;
+
+    return { ...state, ...obj };
+  };
+
+  ctrlState = type => {
+    let { types } = state;
+
+    if (isMulti(type) && !types.has(type)) dispatch({ type });
+
+    if (isMulti(type)) return [
+      useContext(repos.get(type)),
+      value => dispatch({ type, value }),
+    ];
+
+    const singleState = useContext(repos.get(SingleContextType));
+
+    return [
+      singleState?.[type],
+      val => {
+        let valueObj = {};
+        valueObj[type] = val;
+
+        return dispatch({
+          type: SingleContextType,
+          value: {
+            ...SingleContextType?.[type] || {},
+            ...valueObj,
+          }
+        });
+      }
+    ]
+  }
+
+  function renderTmp([type, store]) {
+    const { Provider } = store;
+
+    return children => createElement(Provider,{value:state[type]},children); 
+  }
+
+  const reposArr = Array.from(repos).map(repo => renderTmp(repo));
+  if (repos.size > 0) return compose(...reposArr, children);
+
+  return children;
+}
+
+export { 
+  StateWrapper, 
+  ctrlState, 
+};
---
+++ util.js
@@ -0,0 +1,170 @@
+/**
+* @description: 常用类型检查
+*/
+export function checkType(param) {
+  if (param === null) return null;
+
+  if (typeof param !== "object") return typeof param;
+
+  const objTypes = [
+    {
+      key: 'map',
+      value: Map
+    },
+    {
+      key: 'weakmap',
+      value: WeakMap,
+    },
+    {
+      key: 'set',
+      value: Set,
+    },
+    {
+      key: 'weakset',
+      value: WeakSet,
+    },
+    {
+      key: 'array',
+      value: Array,
+    },
+    {
+      key: 'date',
+      value: Date,
+    },
+    {
+      key: 'regexp',
+      value: RegExp,
+    },
+    {
+      key: 'error',
+      value: Error,
+    },
+    {
+      key: 'promise',
+      value: Promise,
+    },
+    {
+      key: 'event',
+      value: Event,
+    },
+  ];
+  let type = 'object';
+  objTypes.some(({ key, value }) => {
+    const bool = param instanceof value;
+    if (bool) type = key;
+    return bool;
+  });
+  return type;
+}
+
+export function isType(param, type = 'object') {
+  return checkType(param) === type;
+}
+
+export function assignDeep(...objects) {
+  return objects.reduce((accumulator, currentValue) => {
+    Object.keys(currentValue).forEach(key => {
+      const pVal = accumulator[key];
+      const oVal = currentValue[key];
+
+      // if (Array.isArray(pVal) && Array.isArray(oVal)) {
+      //   accumulator[key] = pVal.concat(...oVal);
+      // } else 
+      if (isType(pVal) && isType(oVal)) {
+        accumulator[key] = assignDeep(pVal, oVal);
+      } else {
+        accumulator[key] = oVal;
+      }
+    });
+
+    return accumulator;
+  }, {});
+}
+
+/**
+ * @description 判断俩个变量关系
+ * @returns
+ *  equal 简单类型 相等
+ *    包含 undefined
+ *  different 不同
+ *  same 非简单类型，内存地址相同
+ *    包含 null
+ *  similar 非简单类型，内存地址不同，数据一致
+*/
+export function variableRelation(...rest) {
+  if (rest.length < 2) throw new Error('Util-variableRelation: Missing parameter!');
+
+  const [param1, param2] = rest;
+
+  if (checkType(param1) !== checkType(param2)) return 'different';
+
+  const deterministicType = (t, p = param1) => isType(p, t);
+
+  if (typeof param1 !== 'object' && typeof param2 !== 'object') {
+
+    if (deterministicType('function')) return String(param1) === String(param2) ? 'equal' : 'different';
+
+    if (isNaN(param1) && isNaN(param2)) return 'equal';
+
+    //includes symbol
+    return param1 === param2 ? 'equal' : 'different';
+  }
+
+  if (param1 === param2) return 'same';
+
+
+
+  if (deterministicType('date') && +param1 === +param2) return 'similar';
+
+  if (deterministicType('regexp') && String(param1) === String(param2)) return 'similar';
+
+  if (deterministicType('array')) {
+    if (param1.length !== param2.length) return 'different';
+
+    if (param1.some((v, i) => !isEqual(v, param2[i]))) return 'different';
+
+    return 'similar';
+  }
+
+  if (deterministicType('object')) {
+    const param1Keys = Object.getOwnPropertyNames(param1);
+    const param2Keys = Object.getOwnPropertyNames(param2);
+
+    if (param1Keys.length !== param2Keys.length) return 'different';
+
+    if (param1Keys.some((v, i) => !isEqual(param1[v], param2[v]))) return 'different';
+
+    return 'similar';
+  }
+
+  return 'different';
+
+}
+
+export function isEqual(...rest) {
+  return variableRelation(...rest) !== 'different';
+}
+
+export function compose(...rest) {
+  if (rest.length === 0) return;
+  if (rest.length === 1) {
+    const [param] = rest;
+    return typeof param === 'function' ? param() : param;
+  }
+
+  return rest.reverse().reduce((acc, fn) => fn(acc));
+}
+
+export function mergeMap(...rest) {
+  return new Map(rest.reduce((acc, item) => [...acc, ...item], []));
+}
+
+export default {
+  checkType,
+  isType,
+  assignDeep,
+  variableRelation,
+  isEqual,
+  compose,
+  mergeMap,
+}
