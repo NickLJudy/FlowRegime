ba43350e147f979f1760a74c2ce7bde2aae5069d
yangguang26 <yangguang26@maoyan.com>
2021-06-25 16:39:14 +0800

    bugfixed:import error

--- rollup.config.js
+++ rollup.config.js
@@ -4,33 +4,47 @@ import pkg from './package.json';
 const input = 'src/index.js';
 const external = Object.keys(pkg.peerDependencies || {});
 const env = process.env.BUILD;
-const ProdEnv = env === 'production'; 
+const ProdEnv = env === 'production';
 const CompressionPrefix = ProdEnv ? '.min' : '';
 const plugins = [terser(
-  ProdEnv ? 
-  {
-    compress: {
-      pure_getters: true,
-      unsafe: true,
-      unsafe_comps: true,
-      warnings: false,
-    },
-  }:''
+  ProdEnv ?
+    {
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+    } : ''
 )];
-const output = [
-  {
-    file: `./dist/flowregime.esm${CompressionPrefix}.js`,
-    format: 'esm',
-  },
-  {
-    file: `./dist/flowregime${CompressionPrefix}.js`,
-    format: 'cjs',
-  },
-];
 
-export default {
-  input,
-  external,
-  plugins,
-  output,
-}
+export default [
+  //CommonJS
+  {
+    input,
+    output: { file: `lib/flowregime${CompressionPrefix}.js`, format: 'cjs', indent: false },
+    external,
+    plugins,
+  },
+
+  //ESM
+  {
+    input,
+    output: { file: `esm/flowregime${CompressionPrefix}.js`, format: 'esm', indent: false },
+    external,
+    plugins,
+  },
+
+  //UMD
+  {
+    input,
+    output: {
+      file: `dist/flowregime${CompressionPrefix}.js`,
+      format: 'umd',
+      name: 'FlowRegime',
+      indent: false
+    },
+    external,
+    plugins,
+  },
+]
